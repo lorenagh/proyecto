f905289236cf923103d8296fd04a09a47601af85
NicoGalvarino <70649846+NicoGalvarino@users.noreply.github.com>
2022-04-01 13:08:04 -0300

    hacer pip-instalable

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selfcare_bot",
+    version="0.0.1",
+    author="Lore, Fran y Nico",
+    # author_email="autor@ejemplo.com",
+    description="Te recomienda qué hacer según tus hábitos",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/lorenagh/selfcare_bot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
