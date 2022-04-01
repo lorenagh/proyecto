3f7d42728b02cebf7ebbc62255b9d7fcfd250087
NicoGalvarino <70649846+NicoGalvarino@users.noreply.github.com>
2022-04-01 16:14:52 -0300

    add color + small fixes

--- proyecto.py
+++ proyecto.py
@@ -11,24 +11,17 @@ class persona(object):
         pass
 
 class contexto(persona):
-    # nombre
-    # sentimiento
-    # horas de sueño noche anterior
-    # comida
-    # vasos de agua
-    # metas del día
-    # actividad física
-    # recreación
-    def __init__(self, nombre, sentimiento, hobbie):
+    def __init__(self, nombre, sentimiento, hobbie, color):
         super().__init__()
-        self.nombre = nombre  # name of the person
+        self.nombre = nombre
         self.sentimiento = sentimiento
         self.hobbie = hobbie
-        self.sueño = -1.0
+        self.color = color
+        self.sueño = -1.0  # horas de sueño noche anterior
         self.comida = 0
         self.agua = 0
-        self.metas = 0
-        self.fisica = 0
+        self.metas = 0  # metas del día
+        self.fisica = 0  # actividad física
         self.recreacion = 0
 
     def Sueño(self, sueño):
@@ -72,37 +65,36 @@ class contexto(persona):
 
         rec_sueño = str()
         if np.float64(self.sueño) < 8 and np.float64(self.sueño) > 0:
-            assert type(self.sueño) == float
-            rec_sueño = 'Considera cuidar tus hábitos de sueño'
+            assert type(self.sueño) == str
+            rec_sueño = 'Considera cuidar tus hábitos de sueño \n'
 
         rec_comida = str()
         if self.comida == 'no':
             assert type(self.comida) == str
-            rec_comida = 'Ve a comer algo rico para reponerte y continuar el día'
+            rec_comida = 'Ve a comer algo rico para reponerte y continuar el día \n'
 
         rec_agua = str()
         if self.agua == 'no':
             rec_agua = str()
-            rec_agua = 'Toma agüita (de uwu) para que estés hidratade'
+            rec_agua = 'Toma agüita (de uwu) para que estés hidratade \n'
 
         rec_metas = str()
         if self.metas == 'no':
             assert type(self.metas) == str
-            rec_metas = 'Tómate un rato para reorganizar tu día. \nFija sólo una meta que quieras lograr hoy'
+            rec_metas = 'Tómate un rato para reorganizar tu día, \nfija sólo una meta que quieras lograr hoy \n'
 
         rec_fisica = str()
         if self.fisica == 'no':
             assert type(self.fisica) == str
-            rec_fisica = 'Ve a caminar un rato para estirar las piernas'
+            rec_fisica = 'Ve a caminar un rato para estirar las piernas \n'
 
         rec_recreacion = str()
         if self.recreacion == 'no':
             assert type(self.recreacion) == str
             assert type(self.hobbie) == str
-            rec_recreacion = 'Tómate un ratito para ' + self.hobbie + ' y disfrutar'
+            rec_recreacion = 'Tómate un ratito para ' + self.hobbie + ' y disfrutar \n'
 
-        recomendacion = rec_sueño + '\n' + rec_agua + '\n' + rec_comida + '\n' + rec_fisica \
-             + '\n' + rec_metas + '\n' + rec_recreacion
+        recomendacion = rec_sueño + rec_agua + rec_comida + rec_fisica + rec_metas + rec_recreacion
         
         return recomendacion
 
@@ -131,7 +123,7 @@ class contexto(persona):
         filename, headers = opener.retrieve(links.iloc[i],path_hrb + "cute_{}.jpg".format(i))
 
         img = mpimg.imread(path_hrb + "cute_{}.jpg".format(i))
-            
+
         cute_img = plt.figure(figsize = (10, 15)) 
         ax = plt.subplot(111)
         plt.imshow(img)
@@ -142,41 +134,39 @@ class contexto(persona):
         print(self.recomendacion())
 
         t = plt.text(0.5, -0.1, self.recomendacion(), transform=ax.transAxes, fontsize=12, 
-                        color='black', ha='center', va='center')
+                        color=self.color, ha='center', va='center')
         t.set_bbox(dict(facecolor='white', alpha=0.8, edgecolor='white', boxstyle="round"))
         plt.axis('off')
         plt.show()
 
 
-
 def inputs():
 
-    nombre = input('¿Cómo te llamas?')
-    sentimiento = input('¿Cómo te sientes hoy? (feliz, triste, cansade, tranquile, ansiose) ')
-    hobbie = input('¿Qué te gusta hacer en tu tiempo libre?')
-    # color = input('Cuál es tu color favorito? (blue, green, red, cyan, magenta, yellow, black)')
-    persona = contexto(nombre, sentimiento, hobbie)
+    nombre = input('¿Cómo te llamas?  ')
+    sentimiento = input('¿Cómo te sientes hoy? (feliz, triste, cansade, tranquile, ansiose)  ')
+    hobbie = input('¿Qué te gusta hacer en tu tiempo libre?  ')
+    color = input('Cuál es tu color favorito? (blue, green, red, magenta, black)  ')
+    persona = contexto(nombre, sentimiento, hobbie, color)
 
-    f_sueño = input('¿Cuántas horas dormiste anoche aproximadamente?')
+    f_sueño = input('¿Cuántas horas dormiste anoche aproximadamente?  ')
 
     persona.sueño = f_sueño
 
-    f_comida = input('¿Has comido bien hoy? (si, no)')
+    f_comida = input('¿Has comido bien hoy? (si, no)  ')
     persona.comida = f_comida
 
-    f_agua = input('¿Has tomado suficiente agua hoy? (si, no)')
+    f_agua = input('¿Has tomado suficiente agua hoy? (si, no)  ')
     persona.agua = f_agua
 
-    f_metas = input('¿Vas al día con tus metas de trabajo hoy? (si, no)')
+    f_metas = input('¿Vas al día con tus metas de trabajo hoy? (si, no)  ')
     persona.metas = f_metas
 
-    f_fisica = input('¿Has hecho actividad física hoy? (si, no)')
+    f_fisica = input('¿Has hecho actividad física hoy? (si, no)  ')
     persona.fisica = f_fisica
 
-    f_recreacion = input('¿Has hecho alguna actividad recreativa hoy? (si, no)')
+    f_recreacion = input('¿Has hecho alguna actividad recreativa hoy? (si, no)  ')
     persona.recreacion = f_recreacion
 
-    #persona.
     persona.plot(sentimiento)
 
 inputs()
