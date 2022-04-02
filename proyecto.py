f0dfb356325d4e98cd9f788f8d83c4cfcf3a3dd9
NicoGalvarino <70649846+NicoGalvarino@users.noreply.github.com>
2022-04-01 23:50:08 -0300

    añade idioma inglés

--- proyecto.py
+++ proyecto.py
@@ -11,12 +11,13 @@ class persona(object):
         pass
 
 class contexto(persona):
-    def __init__(self, nombre, sentimiento, hobbie, color):
+    def __init__(self, nombre, sentimiento, hobbie, color, idioma):
         super().__init__()
         self.nombre = nombre
         self.sentimiento = sentimiento
         self.hobbie = hobbie
         self.color = color
+        self.idioma = idioma
         self.sueño = -1.0  # horas de sueño noche anterior
         self.comida = 0
         self.agua = 0
@@ -66,36 +67,54 @@ class contexto(persona):
         rec_sueño = str()
         if np.float64(self.sueño) < 8 and np.float64(self.sueño) > 0:
             assert type(self.sueño) == str
-            rec_sueño = 'Considera cuidar tus hábitos de sueño \n'
+            if self.idioma == 'espanol':
+                rec_sueño = 'Considera cuidar tus hábitos de sueño \n'
+            elif self.idioma == 'english':
+                rec_sueño = 'Consider taking care of your sleep habits \n'
 
         rec_comida = str()
         if self.comida == 'no':
             assert type(self.comida) == str
-            rec_comida = 'Ve a comer algo rico para reponerte y continuar el día \n'
+            if self.idioma == 'espanol':
+                rec_comida = 'Ve a comer algo rico para reponerte y continuar el día \n'
+            elif self.idioma == 'english':
+                rec_comida = 'Go eat somthing nice to gain some energy and continue with your day \n'
 
         rec_agua = str()
         if self.agua == 'no':
-            rec_agua = str()
-            rec_agua = 'Toma agüita (de uwu) para que estés hidratade \n'
+            assert type(self.agua) == str
+            if self.idioma == 'espanol':
+                rec_agua = 'Toma agüita (de uwu) para que estés hidratade \n'
+            elif self.idioma == 'english':
+                rec_agua = 'Drink some water so your nice and hydrated \n'
 
         rec_metas = str()
         if self.metas == 'no':
             assert type(self.metas) == str
-            rec_metas = 'Tómate un rato para reorganizar tu día, \nfija sólo una meta que quieras lograr hoy \n'
+            if self.idioma == 'espanol':
+                rec_metas = 'Tómate un rato para reorganizar tu día, \nfija sólo una meta que quieras lograr hoy \n'
+            elif self.idioma == 'english':
+                rec_metas = 'Take some time to reorganise your day, \nset only one goal you want to get done today \n'
 
         rec_fisica = str()
         if self.fisica == 'no':
             assert type(self.fisica) == str
-            rec_fisica = 'Ve a caminar un rato para estirar las piernas \n'
+            if self.idioma == 'espanol':
+                rec_fisica = 'Ve a caminar un rato para estirar las piernas \n'
+            elif self.idioma == 'english':
+                rec_fisica = 'Go take a walk to stretch your legs \n'
 
         rec_recreacion = str()
         if self.recreacion == 'no':
             assert type(self.recreacion) == str
             assert type(self.hobbie) == str
-            rec_recreacion = 'Tómate un ratito para ' + self.hobbie + ' y disfrutar \n'
+            if self.idioma == 'espanol':
+                rec_recreacion = 'Tómate un ratito para ' + self.hobbie + ' y disfrutar \n'
+            elif self.idioma == 'english':
+                rec_recreacion = 'Take some time to ' + self.hobbie + ' and enjoy yourself \n'
 
         recomendacion = rec_sueño + rec_agua + rec_comida + rec_fisica + rec_metas + rec_recreacion
-        
+
         return recomendacion
 
     def imagenes(self, sentimiento):
@@ -103,7 +122,11 @@ class contexto(persona):
         Entrega pandas.dataframe con links del sentimiento correspondiente
         columnas: link, sentimiento
         '''
-        links = pd.read_table('imagenes.txt', sep = ' ')
+        if self.idioma == 'espanol':
+            links = pd.read_table('imagenes_espanol.txt', sep = ' ')
+        elif self.idioma == 'english':
+            links = pd.read_table('imagenes_english.txt', sep = ' ')
+
         links = links[links['sentimiento'] == sentimiento]
         return links
 
@@ -144,29 +167,39 @@ class contexto(persona):
 
 def inputs():
 
-    nombre = input('¿Cómo te llamas?  ')
-    sentimiento = input('¿Cómo te sientes hoy? (feliz, triste, cansade, tranquile, ansiose)  ')
-    hobbie = input('¿Qué te gusta hacer en tu tiempo libre?  ')
-    color = input('¿Cuál es tu color favorito? (blue, green, red, magenta, black)  ')
-    persona = contexto(nombre, sentimiento, hobbie, color)
+    idioma = input('Selecciona tu idioma de preferencia / Select your prefered language (espanol/english)  ')
 
-    f_sueño = input('¿Cuántas horas dormiste anoche aproximadamente?  ')
+    if idioma == 'espanol':
+        nombre = input('¿Cómo te llamas?  ')
+        sentimiento = input('¿Cómo te sientes hoy? (feliz, triste, cansade, tranquile, ansiose)  ')
+        hobbie = input('¿Qué te gusta hacer en tu tiempo libre?  ')
+        color = input('¿Cuál es tu color favorito? (blue, green, red, magenta, black)  ')
+        f_sueño = input('¿Cuántas horas dormiste anoche aproximadamente?  ')
+        f_comida = input('¿Has comido bien hoy? (si, no)  ')
+        f_agua = input('¿Has tomado suficiente agua hoy? (si, no)  ')
+        f_metas = input('¿Vas al día con tus metas de trabajo hoy? (si, no)  ')
+        f_fisica = input('¿Has hecho actividad física hoy? (si, no)  ')
+        f_recreacion = input('¿Has hecho alguna actividad recreativa hoy? (si, no)  ')
+
+    elif idioma == 'english':
+        nombre = input('What is your name?  ')
+        sentimiento = input('How are you feeling today (happy, sad, tired, calm, anxious)  ')
+        hobbie = input('What do you like to do in your free time?  ')
+        color = input('What is your favourite colour? (blue, green, red, magenta, black)  ')
+        f_sueño = input('How many hours did you sleep last night?  ')
+        f_comida = input('Have you eaten well today? (yes, no)  ')
+        f_agua = input('Have you drank enough water today? (yes, no)  ')
+        f_metas = input('Are you on schedule with your goals for today? (yes, no)  ')
+        f_fisica = input('Have you exercised today? (yes, no)  ')
+        f_recreacion = input('Have you recreated yourself today? (yes, no)  ')
+
+    persona = contexto(nombre, sentimiento, hobbie, color, idioma)
 
     persona.sueño = f_sueño
-
-    f_comida = input('¿Has comido bien hoy? (si, no)  ')
     persona.comida = f_comida
-
-    f_agua = input('¿Has tomado suficiente agua hoy? (si, no)  ')
     persona.agua = f_agua
-
-    f_metas = input('¿Vas al día con tus metas de trabajo hoy? (si, no)  ')
     persona.metas = f_metas
-
-    f_fisica = input('¿Has hecho actividad física hoy? (si, no)  ')
     persona.fisica = f_fisica
-
-    f_recreacion = input('¿Has hecho alguna actividad recreativa hoy? (si, no)  ')
     persona.recreacion = f_recreacion
 
     persona.plot(sentimiento)
